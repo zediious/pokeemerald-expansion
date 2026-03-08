d4f74cbc1ea83a8ecb5ffcbbf8b8bd6580f4ed87
Jack <zediious@aol.com>
2026-03-07 23:17:09 -0500

    new trainer.
    will re-use trainers from here, testing.

--- include/constants/opponents.h
+++ include/constants/opponents.h
@@ -859,12 +859,13 @@
 #define TRAINER_LEAF                        852
 #define TRAINER_BRENDAN_PLACEHOLDER         853
 #define TRAINER_MAY_PLACEHOLDER             854
+#define TRAINER_MAGNUS                      855
 
 // NOTE: Because each Trainer uses a flag to determine when they are defeated, there is only space for 9 additional trainers before trainer flag space overflows
 //       More space can be made by shifting flags around in constants/flags.h or changing how trainer flags are handled
 //       MAX_TRAINERS_COUNT can be increased but will take up additional saveblock space
 
-#define TRAINERS_COUNT_EMERALD     855
+#define TRAINERS_COUNT_EMERALD     856
 #define MAX_TRAINERS_COUNT_EMERALD 864
 
 #if IS_FRLG
