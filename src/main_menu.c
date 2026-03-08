8310e1a03642ffa2b40436d847f5727d24c60470
Jack <zediious@aol.com>
2026-03-07 23:16:44 -0500

    speed up getting to intro

--- src/main_menu.c
+++ src/main_menu.c
@@ -1322,7 +1322,7 @@ static void Task_NewGameBirchSpeech_Init(u8 taskId)
     gTasks[taskId].func = Task_NewGameBirchSpeech_WaitToShowBirch;
     gTasks[taskId].tPlayerSpriteId = SPRITE_NONE;
     gTasks[taskId].data[3] = 0xFF;
-    gTasks[taskId].tTimer = 0xD8;
+    // gTasks[taskId].tTimer = 0xD8;
     PlayBGM(MUS_ROUTE122);
     ShowBg(0);
     ShowBg(1);
@@ -1332,12 +1332,23 @@ static void Task_NewGameBirchSpeech_WaitToShowBirch(u8 taskId)
 {
     u8 spriteId;
 
-    if (gTasks[taskId].tTimer)
-    {
-        gTasks[taskId].tTimer--;
-    }
-    else
-    {
+    // if (gTasks[taskId].tTimer)
+    // {
+    //     gTasks[taskId].tTimer--;
+    // }
+    // else
+    // {
+    //     spriteId = gTasks[taskId].tBirchSpriteId;
+    //     gSprites[spriteId].x = 136;
+    //     gSprites[spriteId].y = 60;
+    //     gSprites[spriteId].invisible = FALSE;
+    //     gSprites[spriteId].oam.objMode = ST_OAM_OBJ_BLEND;
+    //     NewGameBirchSpeech_StartFadeInTarget1OutTarget2(taskId, 10);
+    //     // NewGameBirchSpeech_StartFadePlatformOut(taskId, 20);
+    //     gTasks[taskId].tTimer = 80;
+    //     gTasks[taskId].func = Task_NewGameBirchSpeech_WaitForSpriteFadeInWelcome;
+    // }
+
         spriteId = gTasks[taskId].tBirchSpriteId;
         gSprites[spriteId].x = 136;
         gSprites[spriteId].y = 60;
