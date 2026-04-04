9aca26d66f54cacf2bfc06820b5e2ea7a41f5653
Jack <zediious@aol.com>
2026-04-04 18:41:16 -0400

    add saveblock size for save tests

--- test/save.c
+++ test/save.c
@@ -5,8 +5,8 @@
 // If you would like to ensure save compatibility, update the values below with those for your hack. You can find these through the debug menu.
 // Please note that this simple check is not 100% foolproof, but should be able to catch most unintended shifts.
 #define T_SAVEBLOCK1_SIZE 15568
-#define T_SAVEBLOCK2_SIZE 3884
-#define T_SAVEBLOCK3_SIZE 4
+#define T_SAVEBLOCK2_SIZE 3892
+#define T_SAVEBLOCK3_SIZE 1620
 #define T_POKEMONSTORAGE_SIZE 34144
 
 TEST("SaveBlock1 is backwards compatible")
