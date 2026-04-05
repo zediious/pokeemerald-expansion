1bfbe989aaa1c1b8dd9f3a635b34c65b2a987a94
Jack <zediious@aol.com>
2026-04-05 16:12:34 -0400

    npcs to woltia mart
    Includes a dude who tells you about the vending machins, and also a hiker who gives you the avalanche TM.

--- include/constants/flags.h
+++ include/constants/flags.h
@@ -679,7 +679,7 @@
 #define FLAG_UNUSED_0x270  0x270 // Unused Flag
 #define FLAG_GERBON_GOT_MACHO                   0x271 // Player got Macho Brace from Gerbon Town NPC
 #define FLAG_MINT_VENDING_USED                  0x272 // Indicates player has used a mint vending machine at least once
-#define FLAG_UNUSED_0x273  0x273 // Unused Flag
+#define FLAG_WOLTIA_HIKER_AVALANCE              0x273 // Hiker in Woltia mart gave Avalance TM
 #define FLAG_UNUSED_0x274  0x274 // Unused Flag
 #define FLAG_UNUSED_0x275  0x275 // Unused Flag
 #define FLAG_UNUSED_0x276  0x276 // Unused Flag
