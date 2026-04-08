c6e5dc320c405ec2e4050ae15a002832b919ef96
Jack <zediious@aol.com>
2026-04-08 16:21:47 -0400

    Fix wrong jordan trainer slide trainer

--- src/trainer_slide.c
+++ src/trainer_slide.c
@@ -122,7 +122,7 @@ static const u8* const sTrainerSlides[DIFFICULTY_COUNT][TRAINER_PARTNER(PARTNER_
             [TRAINER_SLIDE_LAST_SWITCHIN] = COMPOUND_STRING("I didn't think this could happen!{PAUSE_UNTIL_PRESS}"),
             [TRAINER_SLIDE_PLAYER_LANDS_FIRST_CRITICAL_HIT] = COMPOUND_STRING("Only you have that luck!{PAUSE_UNTIL_PRESS}"),
         },
-        [TRAINER_MAY_ROUTE_103_TREECKO] =
+        [TRAINER_MAY_ROUTE_110_TREECKO] =
         {
             [TRAINER_SLIDE_BEFORE_FIRST_TURN] = COMPOUND_STRING("I've gotten stronger \n{PLAYER}!\nGet ready for this!{PAUSE_UNTIL_PRESS}"),
             [TRAINER_SLIDE_LAST_SWITCHIN] = COMPOUND_STRING("I didn't think this could happen!{PAUSE_UNTIL_PRESS}"),
