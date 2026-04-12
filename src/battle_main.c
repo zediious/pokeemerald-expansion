d2d30509016f5f9345770e721284f56c08128e92
Jack <zediious@aol.com>
2026-04-12 13:56:30 -0400

    prev append trainer class data

--- src/battle_main.c
+++ src/battle_main.c
@@ -422,6 +422,7 @@ const struct TrainerClass gTrainerClasses[TRAINER_CLASS_COUNT] =
     [TRAINER_CLASS_RUIN_MANIAC_FRLG] =     { _("RUIN MANIAC"), 12 },
     [TRAINER_CLASS_LADY_FRLG] =            { _("LADY"), 50 },
     [TRAINER_CLASS_PAINTER_FRLG] =         { _("PAINTER"), 4 },
+    [TRAINER_CLASS_WAITER] =               { _("Waiter"), 20, BALL_NEST },
 };
 
 static void (*const sTurnActionsFuncsTable[])(void) =
