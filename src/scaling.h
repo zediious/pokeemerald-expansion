d0ad3ca7291ac609a7f841cfe5285880d3411509
Jack <zediious@aol.com>
2026-04-12 16:16:12 -0400

    Special to get highest level mon in party
    Uses already existing function in `scaling.c`, which has now been exposes publicly

--- src/scaling.h
+++ src/scaling.h
@@ -6,3 +6,4 @@ struct TrainerMon *ScaleTrainerMons(u32 partySize, struct TrainerMon *scaledPart
 struct TrainerMon EvolveTrainerMon(const struct Evolution *evolutions, struct TrainerMon trainerMon, u8 levelCeil, u8 evolutionCount);
 struct TrainerMon EvolveBranchTrainerMon(const struct Evolution *evolutions, struct TrainerMon trainerMon, u8 levelCeil, u8 evolutionCount);
 struct TrainerMon EvolveParentTrainerMon(const struct Evolution *evolutions, const struct Evolution *parentEvolutions, struct TrainerMon trainerMon, u32 evoIndex, u8 levelCeil);
+u8 GetPlayerLevelCeiling(void);
