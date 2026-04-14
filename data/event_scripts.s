f8cbdefe39513388c1b0d02eb81380456582c842
Jack <zediious@aol.com>
2026-04-14 17:07:50 -0400

    liordun base and connection to route 104
    Plenty of edits in both tilesets, liordun town tileset is not made yet.

--- data/event_scripts.s
+++ data/event_scripts.s
@@ -1835,3 +1835,5 @@ EventScript_PalletTown_PlayersHouse_2F_TurnOnPC::
 	.include "data/maps/GerbonTown_FlatFloor02/scripts.inc"
 
 	.include "data/maps/GerbonTown_Restaraunt/scripts.inc"
+
+	.include "data/maps/Vulias_Liordun_Town/scripts.inc"
